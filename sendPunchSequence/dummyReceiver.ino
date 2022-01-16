9af1e409a46bc64912c5171e2775a982747f7aa0
Jan729 <>
2022-01-15 21:15:33 -0500

    Fix bit ordering

--- sendPunchSequence/dummyReceiver.ino
+++ sendPunchSequence/dummyReceiver.ino
@@ -1,45 +1,51 @@
-//Source: https://www.arduino.cc/en/Tutorial/LibraryExamples/MasterWriter
+// Wire Slave Receiver
+// by Nicholas Zambetti <http://www.zambetti.com>
 
-//Endlessly waits for data from I2C bus and prints out the data it received
+// Demonstrates use of the Wire library
+// Receives data as an I2C/TWI slave device
+// Refer to the "Wire Master Writer" example for use with this
+
+// Created 29 March 2006
+
+// This example code is in the public domain.
+
+//https://www.arduino.cc/en/Tutorial/LibraryExamples/MasterWriter
 
 #include <Wire.h>
 #define NUM_BYTES_PER_ROW 4
 
 void setup()
 {
-  Wire.begin(4);                // join i2c bus with address #4
-  Wire.onReceive(receiveEvent); // register event
-  Serial.begin(9600);           // start serial for output
-  Serial.println("I am receipt printer arduino");
-  
-  
+    Wire.begin(4);                // join i2c bus with address #4
+    Wire.onReceive(receiveEvent); // register event
+    Serial.begin(9600);           // start serial for output
+    Serial.println("I am receipt printer arduino");
 }
 
 void loop()
 {
-  delay(100);
+    delay(1000);
 }
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
 void receiveEvent(int howMany)
 {
-  int byteIdx = 1;
-  //FIXME bytes received are not in order
-  while(1 <= Wire.available())
-  {
-    byte i = Wire.read();
-    Serial.print(i, BIN);
-    Serial.print(" ");
-    if(byteIdx == NUM_BYTES_PER_ROW) {
-      byteIdx = 1;
-      Serial.println();
-      //Serial.print("Wire.available(): ");
-      //Serial.print(Wire.available());
-    } else {
-      byteIdx++;
-    }      
-  }
-  
-  Serial.println();
+    int rowIdx = 1;
+
+    while (1 <= Wire.available())
+    {
+        byte data = Wire.read();
+
+        Serial.print(data, BIN);
+        if (rowIdx == NUM_BYTES_PER_ROW)
+        {
+            rowIdx = 1;
+            Serial.println();
+        }
+        else
+        {
+            rowIdx++;
+        }
+    }
 }
--- sendPunchSequence/sendPunchSequence.ino
+++ sendPunchSequence/sendPunchSequence.ino
@@ -26,87 +26,88 @@ boolean leds[NUM_STRIPS][NUM_LEDS_PER_STRIP];
 
 void setup()
 {
-  Wire.begin(); // join i2c bus (address optional for master)
+    Wire.begin(); // join i2c bus (address optional for master)
+    Serial.begin(9600);
+    Serial.println("I am etch a sketch arduino");
 
-  Serial.begin(9600);
-  Serial.println("I am etch a sketch arduino");
-  
-  for (int row=0; row<NUM_STRIPS; row++)
-  {
-    for (int col=0; col<NUM_LEDS_PER_STRIP; col++) {
-      leds[row][col] = col % 2;
+    for (int row = 0; row < NUM_STRIPS; row++)
+    {
+        for (int col = 0; col < NUM_LEDS_PER_STRIP; col++)
+        {
+            leds[row][col] = 1;
+        }
     }
-  }
-  
-  //CreateDummyPicture();
+
+    CreateDummyPicture();
 }
 
-void loop() {
-  SendPunchSequence();
-  
-  delay(2000);
+void loop()
+{
+    SendPunchSequence();
+
+    delay(5000);
 }
 
+// Send all 256 pixels as a 1 or 0
 void SendPunchSequence()
 {
-  Wire.beginTransmission(4); // transmit to device #4
-    
-  // idea #1: to compress the image to 16x16,
-  // imagine a large pixel is 2x2 leds.
-  // We send the value at the top left corner 
-  // of each large pixel
-  // (we'll need to revise this during prototyping)
-  for (int row=0; row<NUM_STRIPS; row+=2)
-  {
-    // assuming each led value is either 0 or 1,
-    //write 1 byte (aka 8 bits) of data at a time
-    for (int col=0; col<NUM_LEDS_PER_STRIP; col+=8) {
-      byte compressedBit = CompressedColourData(row, col);
-      Wire.write(compressedBit);
-      Serial.print(compressedBit, BIN);
-      Serial.print(" ");
+    for (int row = 0; row < NUM_STRIPS; row++)
+    {
+        Wire.beginTransmission(4); // transmit one row of data to device #4
+
+        // After mapping 8 pixel values to 0 or 1,
+        //write 1 byte of data at a time
+
+        for (int col = 0; col < NUM_LEDS_PER_STRIP; col += 8)
+        {
+            byte compressedBits = CompressedColourData(row, col);
+            Wire.write(compressedBits);
+            Serial.print(compressedBits, BIN);
+        }
+        Serial.println();
+        if (Wire.endTransmission() != 0)
+        {
+            Serial.println("Error transmitting!");
+        }
     }
-    Serial.println();
-  }
-  
-   Serial.println();
-   Serial.println();
-   Serial.println();
-  
-  if(Wire.endTransmission() != 0) {    // stop transmitting
-	Serial.println("Error transmitting");
-  }
 }
-  
-  byte CompressedColourData(int row, int col) {  
-    byte colour = B00000000;
 
-    for (int i=0; i<8; i++) {
-        byte nextBit = leds[row][col+i] > 0 ? B00000001 : B00000000;
-        colour = colour | nextBit;
-        colour << 1;
+byte CompressedColourData(int row, int col)
+{
+    byte data = 0;
+
+    for (int i = 0; i < 8; i++)
+    {
+        if (leds[row][col + i])
+        {
+            data |= (1 << i);
+        }
     }
+    return data;
+}
 
-    return colour;
-  }
+void CreateDummyPicture()
+{
+    //left eye
+    leds[4][3] = 0;
+    leds[4][4] = 0;
+    leds[5][3] = 0;
+    leds[5][4] = 0;
 
-void CreateDummyPicture() {
-  //left eye
-  leds[4][2] = 1;
-  leds[4][4] = 1;
-  leds[6][2] = 1;
-  leds[6][4] = 1;
-  
-  //right eye
-  leds[4][28] = 1;
-  leds[4][26] = 1;
-  leds[6][28] = 1;
-  leds[6][26] = 1;
-  
-  //mouth
-  leds[20][2] = 1;
-  leds[20][28] = 1;
-  for(int col = 4; col<=26; col+=2) {
-    leds[22][col] = 1;
-  }
+    //right eye
+    leds[4][27] = 0;
+    leds[4][28] = 0;
+    leds[5][27] = 0;
+    leds[5][28] = 0;
+
+    //mouth
+    for (int col = 7; col <= 21; col++)
+    {
+        leds[10][col] = 0;
+        leds[11][col] = 0;
+    }
+    for (int col = 7; col <= 21; col++)
+    {
+        leds[10][col] = col % 2;
+    }
 }
