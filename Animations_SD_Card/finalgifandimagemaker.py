37b8c93163105a09c22ac2a1f8f95ee107acf220
Eridarus <56792465+Eridarus@users.noreply.github.com>
2022-05-06 00:11:03 -0400

    Update finalgifandimagemaker.py
    Added handling for batches of images and made it (hopefully) not crash if transparent pixels are in the gifs/images. See the README comment at the top for instructions on how to use it, message me (Reece) if there are errors.

--- Animations_SD_Card/finalgifandimagemaker.py
+++ Animations_SD_Card/finalgifandimagemaker.py
@@ -1,13 +1,29 @@
 
-##README: If you want to write an image, use img2txt(image_filename, output textfile filename). Jpegs and pngs should both work. If you want to write a gif, use gif2txt(gif filename, output textfile filename). output filename has to have .txt cause i don't have it in me to format any more strings thatn I already have
+##README: If you want to write an image, use img2txt(image_filename, output textfile filename). Jpegs and pngs should both work. If you want to write a gif, use gif2txt(gif filename, output textfile filename). output filename has to have .txt cause i don't have it in me to format any more strings than I already have
+#If you have several gifs and images you want to do at the same time, put use handle_batch(folder with files to convert, folder where you want to store the outputs,gifmode=mode you want to convert gifs with (optional),imgmode=mode you want to convert static images with (optional)). Note that you don't get to name files individually this way - whatever you name the original file will be used (with static or ani to denote if it's a static gif or animation). Transparent pixels get turned to black (which translates to the LEDs on the strips being just off); nonsquare gifs will get auto-resized so they'll probably either look really ugly or just not work, so you should resize them before use
+#If it runs into an error transcribing the image, it will delete the txt file it made to avoid any issues on the actual system (can't imagine why this would happen though)
 from PIL import Image
 from PIL import ImageFile
 import PIL
 import numpy
 import sys
+import os
 
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+def expand2square(pil_img, background_color):
+    width, height = pil_img.size
+    if width == height:
+        return pil_img
+    elif width > height:
+        result = Image.new(pil_img.mode, (width, width), background_color)
+        result.paste(pil_img, (0, (width - height) // 2))
+        return result
+    else:
+        result = Image.new(pil_img.mode, (height, height), background_color)
+        result.paste(pil_img, ((height - width) // 2, 0))
+        return result
 
 def resize_image(image, filename="", autosave=True, image_mode=Image.Resampling.NEAREST):
     height = image.height
@@ -18,6 +34,7 @@ def resize_image(image, filename="", autosave=True, image_mode=Image.Resampling.
     return resized
 
 def write_image_text(image, filename, printO=False, fix=True, autoclose=True, filemode='w'):
+    #image=expand2square(pre_image, (255,255,255))
     data=numpy.asarray(image)
     f=open(filename, filemode)
     count=0
@@ -84,7 +101,7 @@ def save_new_gif(new_frames, old_gif_information, new_path):
 
 def write_gif_text(gif, filename, delay=9, printO=False, fix=True, image_mode=Image.Resampling.LANCZOS):
     new_frames=get_new_frames(gif, (32,32), image_mode=image_mode)
-    delay=gif.info.get('duration')
+    delay=min(gif.info.get('duration'),100)
     if delay==13:
         delay=12
     if delay==10:
@@ -105,12 +122,36 @@ def write_gif_text(gif, filename, delay=9, printO=False, fix=True, image_mode=Im
 def image2txt(input_file, output_file, image_mode=Image.Resampling.NEAREST, printO=False):
     resized=resize_image(Image.open(input_file), autosave=False, image_mode=image_mode)
     write_image_text(resized, output_file, printO)
-    print("All done!")
+    print("All done with "+input_file+"!")
 
 def gif2txt(input_file, output_file, image_mode=Image.Resampling.LANCZOS, printO=False):
     write_gif_text(Image.open(input_file), output_file, image_mode=image_mode, printO=printO)
-    print("All done!")
+    print("All done with "+input_file+"!")
 
-# gif2txt("splash_XS_frame_data.gif", "splash_XS_frame_data.txt", image_mode=Image.Resampling.BOX)
-gif2txt("splash_xxl_frame_data_filled.gif", "splash_xxl_frame_data_filled.txt", image_mode=Image.Resampling.BOX)
-#image2txt("Utoronto_coa.png","finaltxts/utlogo-static.txt", image_mode=Image.Resampling.BOX)
+def handle_batch(input_folder, output_folder, gifmode=Image.Resampling.LANCZOS, imgmode=Image.Resampling.NEAREST, printO=False):
+    files=os.listdir(input_folder)
+    files_to_kill=[]
+    for filename in files:
+        try:
+            name=filename.split('.')[0]
+            extension=filename.split('.')[1]
+            if extension=="gif":
+                gif2txt(input_folder+"/"+filename, output_folder+"/"+name+"-ani.txt", image_mode=gifmode, printO=printO)
+            elif extension=="png" or extension=="jpg":
+                image2txt(input_folder+"/"+filename, output_folder+"/"+name+"-static.txt", image_mode=imgmode, printO=printO)
+            else:
+                print("Unsupported filetype for file "+filename)
+        except UnicodeEncodeError as uee:
+            print(uee)
+            if extension=="gif":
+                files_to_kill.append(output_folder+"/"+name+"-ani.txt")
+            else:
+                files_to_kill.append(output_folder+"/"+name+"-static.txt")
+            continue
+    for k in files_to_kill:
+        os.remove(k)
+
+
+#gif2txt("batches/earthspin.gif", "finaltxts/earthspin-ani.txt", image_mode=Image.Resampling.NEAREST)
+#image2txt("batches/sparklogo.jpg","finaltxts/sparklogo-static.txt", image_mode=Image.Resampling.BICUBIC)
+handle_batch("batches","finaltxts", gifmode=Image.Resampling.BOX, imgmode=Image.Resampling.LANCZOS)
