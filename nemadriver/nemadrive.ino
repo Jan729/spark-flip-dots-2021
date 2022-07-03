3dfb27e47ff231b972506adc0e3de24da03e518f
Eridarus <56792465+Eridarus@users.noreply.github.com>
2022-07-03 10:17:30 -0400

    Create nemadrive.ino

---
+++ nemadriver/nemadrive.ino
@@ -0,0 +1,49 @@
+// Define pin connections & motor's steps per revolution
+const int dirPin = 2;
+const int stepPin = 3;
+const int stepsPerRevolution = 600;
+
+void setup()
+{
+  // Declare pins as Outputs
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+}
+void loop()
+{
+  // Set motor direction clockwise
+  digitalWrite(dirPin, HIGH);
+
+  // Spin motor slowly
+  for(int x = 0; x < stepsPerRevolution; x++)
+  {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(2000);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(2000);
+  }
+  delay(1000); // Wait a second
+  
+  // Set motor direction counterclockwise
+  digitalWrite(dirPin, LOW);
+
+  // Spin motor quickly
+  for(int x = 0; x < stepsPerRevolution; x++)
+  {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(1000);
+  }
+  delay(1000); // Wait a second
+}
+
+void driveMotor(int dir, int steps, int tdelay){
+  digitalWrite(dirPin, dir);
+  for(int x=0; x<steps; x++){
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(tdelay);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(tdelay);
+  }
+}
